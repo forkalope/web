8a49bf8611770f21e484334f2c3d408a8d2dc760
aa <oneone@gmail.com>
2026-09-13 08:50:38 -0700

    new look of forkalope

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener("click", () => {
+    const isOpen = menuToggle.getAttribute("aria-expanded") === "true";
+    menuToggle.setAttribute("aria-expanded", String(!isOpen));
+    mobileMenu.hidden = isOpen;
+  });
+
+  mobileMenu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      menuToggle.setAttribute("aria-expanded", "false");
+      mobileMenu.hidden = true;
+    });
+  });
+}
